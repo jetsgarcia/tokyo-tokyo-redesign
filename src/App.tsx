801f7155aac467b2080e46b0935b9bc67031223b
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-09 18:34:00 +0800

    Add route for menu page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router";
 import Home from "@/pages/home/Home";
-import Navbar from "./components/Navbar";
+import Navbar from "@/components/Navbar";
+import Menu from "@/pages/menu/Menu";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
       </Routes>
     </div>
   );

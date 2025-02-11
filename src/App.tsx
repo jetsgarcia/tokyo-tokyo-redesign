1daf823d9f102655a2d3a5d8894441d786e394f1
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-11 16:25:58 +0800

    Add route for a bento food

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,21 @@ import Home from "@/pages/home/Home";
 import Navbar from "@/components/Navbar";
 import Menu from "@/pages/menu/Menu";
 import Bento from "@/pages/menu/subroutes/bento/BentoSection";
+import PrawnAndVeggieTempuraBento from "./pages/menu/subroutes/bento/prawn-and-veggie-tempura-bento/PrawnAndVeggieTempuraBento";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <div className="mt-[16.95vw] pb-24 md:mt-[85px] md:pb-0">
+      <div className="mt-[69.9px] pb-24 md:mt-[85px] md:pb-0">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/menu" element={<Menu />} />
           <Route path="/menu/bento" element={<Bento />} />
+          <Route
+            path="/menu/bento/prawn-and-veggie-tempura-bento"
+            element={<PrawnAndVeggieTempuraBento />}
+          />
         </Routes>
       </div>
     </div>
---
+++ src/pages/menu/subroutes/bento/prawn-and-veggie-tempura-bento/PrawnAndVeggieTempuraBento.tsx
@@ -0,0 +1,3 @@
+export default function PrawnAndVeggieTempuraBento() {
+  return <div></div>;
+}

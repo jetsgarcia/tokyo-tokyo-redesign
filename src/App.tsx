8c0e2ac113db93796de20c38fa711e2edafd6ba4
Jetter <jlgarcia2@student.apc.edu.ph>
2025-02-23 09:50:56 +0800

    Add NotFound route to handle undefined paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/Navbar";
 import Menu from "@/pages/menu/Menu";
 import BentoList from "@/pages/menu/bento/BentoList";
 import BentoDetails from "@/pages/menu/bento/BentoDetails";
+import NotFound from "./components/NotFound";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Navbar />
       <div className="mt-[69.9px] pb-24 md:mt-[69px] md:pb-0">
         <Routes>
+          <Route path="*" element={<NotFound />} />
           <Route path="/" element={<Home />} />
           <Route path="menu" element={<Menu />} />
           <Route path="/menu/bento" element={<BentoList />} />
